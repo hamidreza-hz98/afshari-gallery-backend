88246bbca28aa9785836ee1a1107f91cf9dd0c0d
Hamidreza Hassanzadeh <hamidrezahz1999@gmail.com>
2025-12-03 12:35:40 +0330

    added new cors policy

--- settings/config.js
+++ settings/config.js
@@ -17,8 +17,10 @@ module.exports = function (app, express) {
       origin: [
         "http://localhost:3000",
         "http://localhost:3001",
+        "http://localhost:3002",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "http://127.0.0.1:3002",
 
         "https://afsharigallery.ir",
         "https://www.afsharigallery.ir",
